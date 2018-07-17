e3f7edb5591254b77be1958133818ee0938baa4a
hennasiddiq <homa.rahimi@aol.com>
2018-07-17 21:07:55 +0000

    random message

--- app/models/sample_model.rb
+++ app/models/sample_model.rb
@@ -1,3 +1,6 @@
-class SampleModel
-  attr_accessor :name, :age
-end
+def kwiz_return
+  if response1 == "array" && response2 == "pancakes"
+    "WEIRD YOU are an arrat who likes pancakes!"
+  elsif response1 == "array" && response2 == "waffles"
+  "OOOOOOOHHHHH SPICCCYYYYYY"
+  elsif response2
